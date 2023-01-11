f2a6ef4d3b38edd022b5e7ac2779cc5b4ff12edc
wooshdude <parker@cline.cc>
2023-01-11 02:31:29 -0800

    slash command boilerplate

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+'''
+    NEOdrew discord bot. Created by thirdoul.
+'''
 
+# Discord-related libraries
+import discord
+from discord import app_commands
 
-def main():
-    pass
+# Base libraries
+import time
+import datetime
+import random
 
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
-if __name__ == "__main__":
-    main()
+@tree.command(name='ping', description='ping pong', guild=discord.Object(id=587471529118531595))
+async def ping(interaction):
+    await interaction.response.send_message("pong!")
+
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=587471529118531595))
+    print(f"Client ready!")
+
+client.run('ODM1NzQwMzMwMTExOTI2MzEy.GX1Dgn.tmBv3cIUA8TpV1znZaws9dipy8hwDZjgEJvfPg')
