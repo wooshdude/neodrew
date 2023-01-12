20c187b93d8ae1b9e9a69352b65ebddcc6ceee07
wooshdude <parker@cline.cc>
2023-01-11 16:47:35 -0800

    added poll generator

--- main.py
+++ main.py
@@ -10,11 +10,13 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from discord.utils import get
 
 # Base libraries
 import time
 import datetime
 import random
+
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -22,32 +24,42 @@ tree = app_commands.CommandTree(client)
 
 
 class Partners:
+    def add(self):
+        return discord.Object(id=self)
+
     artism = 587471529118531595
     omescape = 1039442477079347261
 
+
 # Command for testing. Will respond with 'pong' on use.
-@tree.command(name='ping', description='ping pong', guild=discord.Object(id=Partners.artism))
+@tree.command(name='ping', description='ping pong', guild=Partners.add(Partners.artism))
 async def ping(interaction):
     await interaction.response.send_message("pong!")
 
 
 # Drew commands
-
-
+@tree.command(name='poll', description='Start a new poll. Up and down vote reactions will be automatically added.', guild=Partners.add(Partners.artism))
+async def poll(interaction, *, question: str):
+    start = time.time()
+    await interaction.response.send_message(f"`Public poll`")
+    msg = await interaction.channel.send(f'> {question}')
+    reactions = ['<a:upvote:837521121447510018>', '<a:downvote:837517888420053003>']
+    for emoji in reactions:
+        await msg.add_reaction(emoji)
 
 
 # All help commands
 @tree.command()
-async def help(interaction):
+async def help_commands(interaction):
     await interaction.response.send_message("help command!")
 
-group = app_commands.Group(name='help', description='View helpfull command information!')
+help = app_commands.Group(name='help', description='View helpfull command information!')
 
-@group.command()
+@help.command()
 async def ping(interaction):
     await interaction.response.send_message("command for debugging. should reply with 'pong'")
 
-tree.add_command(group, guild=discord.Object(id=Partners.artism))
+tree.add_command(help, guild=discord.Object(id=Partners.artism))
 
 
 
