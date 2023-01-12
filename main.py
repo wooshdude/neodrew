9cd5b14813c9efd4491599a2cd617bf5f042982b
wooshdude <parker@cline.cc>
2023-01-11 17:05:23 -0800

    added role selector

--- main.py
+++ main.py
@@ -48,6 +48,41 @@ async def poll(interaction, *, question: str):
         await msg.add_reaction(emoji)
 
 
+@tree.command(name='roles', description='Summon a role selector.', guild=Partners.add(Partners.artism))
+async def roles(interaction):
+    await interaction.response.send_message("Select a role.", view=RoleSelectView(), ephemeral=True)
+    
+
+# Buttons and menus
+class RoleSelect(discord.ui.Select):
+    def __init__(self):
+        options=[
+            discord.SelectOption(label="Destiny 2",description="For all things that game."),
+            discord.SelectOption(label="Minecraft",description="Access to the Minecraft server"),
+            discord.SelectOption(label="Overwatch",description="winton")
+            ]
+        super().__init__(placeholder="Select an option",max_values=1,min_values=1,options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        if self.values[0] == "Destiny 2":
+            role = interaction.guild.get_role(587473995155374111)
+            await interaction.user.add_roles(role)
+            await interaction.response.send_message(f"Aquired new role <@&587473995155374111>", ephemeral=True)
+        elif self.values[0] == "Option 2":
+            role = interaction.guild.get_role(728352636713173002)
+            await interaction.user.add_roles(role)
+            await interaction.response.send_message(f"Aquired new role <@&728352636713173002>", ephemeral=True)
+        elif self.values[0] == "Option 3":
+            role = interaction.guild.get_role(1026888371710214154)
+            await interaction.user.add_roles(role)
+            await interaction.response.send_message(f"Aquired new role <@&1026888371710214154>", ephemeral=True)
+
+class RoleSelectView(discord.ui.View):
+    def __init__(self, *, timeout = 180):
+        super().__init__(timeout=timeout)
+        self.add_item(RoleSelect())
+
+
 # All help commands
 @tree.command()
 async def help_commands(interaction):
